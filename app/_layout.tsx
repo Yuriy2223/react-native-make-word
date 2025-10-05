20079733fe7a0f94405ec349b07d5d414d4aadde
Yuriy <>
2025-10-05 23:50:21 +0300

    working with the nav bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,39 @@
+// import { Stack } from "expo-router";
+// import { StyleSheet, View } from "react-native";
+
+// export default function RootLayout() {
+//   return (
+//     <View style={styles.container}>
+//       <Stack screenOptions={{ headerShown: false }}>
+//         <Stack.Screen name="(tabs)" />
+//       </Stack>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+// });
+import * as NavigationBar from "expo-navigation-bar";
 import { Stack } from "expo-router";
-import { StyleSheet, View } from "react-native";
+import { useEffect } from "react";
+import { Platform, StyleSheet, View } from "react-native";
 
 export default function RootLayout() {
+  useEffect(() => {
+    if (Platform.OS === "android") {
+      NavigationBar.setVisibilityAsync("hidden");
+
+      NavigationBar.setBehaviorAsync("inset-swipe");
+
+      NavigationBar.setBackgroundColorAsync("#667eea");
+
+      NavigationBar.setButtonStyleAsync("light");
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
       <Stack screenOptions={{ headerShown: false }}>
